44e803d18c86be7db7e9ad710bb2a8f9fc49415f
zbolander <bdrewfb@gmail.com>
2013-10-14 17:48:26 -0400

    just testing git software

--- example_list.py
+++ example_list.py
@@ -2,6 +2,8 @@
 this is the stuff specific to the plant studies that are loaded. It can include comments and all that. The studytreelist will be read from the load_synth_extract file. Other variables in the conf could be overridden here. 
 """
 
+#test comment
+
 synthottolid="10218"
 
 studytreelist=["562_817",
