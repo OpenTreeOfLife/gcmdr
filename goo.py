0abf9f30517e7306b0c78d31637b207393d0da83
josephwb <phylo.jwb@gmail.com>
2013-12-16 11:09:41 -0500

    add gitignore

--- goo.py
+++ goo.py
@@ -1,10 +1,12 @@
-synthottolid="691846"
+# a dummy file to run specific tests of problematic taxa
 
 studytreelist=[
                "420_522",   # Aves. Hackett et al. 2008. Science
                "1428_2855", # Mammals. Meredith et al. 2011. Science
                "2573_5959", # Sauria. Crawford et al. 2012. Biology Letters
             ]
+
+studytreelistTF = [True] * len(studytreelist)
 
 if __name__ == "__main__":
 	import load_synth_extract
@@ -16,5 +18,5 @@ if __name__ == "__main__":
 	print "loading studytreelist:",studytreelist
 
 	load_synth_extract.run(dott,dload,studyloc,studytreelist,javapre,
-				 treemloc,generallogfileloc,dsynth,synthottolid,treefn)
+				 treemloc,generallogfileloc,dsynth,synthottolid,treefn,studytreelist)
 
