435cce054ec0ce89eb8efa9d5f144478daf56464
Stephen Smith <blackrim@gmail.com>
2014-01-28 12:09:43 -0500

    minor

--- load_studies.py
+++ load_studies.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     from stephen_desktop_conf_TEMP import *
     
     synthottolid="10218"
-    studytreelist = ["261_145"]
+    studytreelist = ["225_5991"]
     print "loading synthottolid:",synthottolid
     print "loading studytreelist:",studytreelist
     
