665073b734a9885482c9733bc11cdddd97201c80
Stephen Smith <blackrim@gmail.com>
2014-02-12 16:55:13 -0500

    tax

--- process_taxonomy_for_load_log.py
+++ process_taxonomy_for_load_log.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     stack = [targetid]
     while len(stack) > 0:
         tempid = stack.pop()
-        outfile.write(tempid+"\t|\t"+pid[tempid]+"\t|\t"+nid[tempid]+"\t|\t"+nrank[tempid]+"\t|\t"+sid[tempid]+"\t|\t"+unid[tempid]+"\t|\t"+flagsp[tempid]+"\t|\t"\n")
+        outfile.write(tempid+"\t|\t"+pid[tempid]+"\t|\t"+nid[tempid]+"\t|\t"+nrank[tempid]+"\t|\t"+sid[tempid]+"\t|\t"+unid[tempid]+"\t|\t"+flagsp[tempid]+"\t|\t\n")
         if tempid in cid:
             for i in cid[tempid]:
                 stack.append(i)
