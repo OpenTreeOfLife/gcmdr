1eb2cca872744c128971e88ded5343606dbaaaee
Stephen Smith <blackrim@gmail.com>
2014-02-15 15:20:38 -0500

    testing

--- load_studies_inf_mono.py
+++ load_studies_inf_mono.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
     from stephen_desktop_conf_TEMP import *
     
     synthottolid="10218"
-    #studytreelist = ["2539_6294"]
+    studytreelist = ["2539_5465"]
     print "loading synthottolid:",synthottolid
     print "loading studytreelist:",studytreelist
     
     for i in studytreelist:
         tstudy_list = [i]
-        generallogfileloc = "plantslog/"+i+".log"
-        ttfntreefn = "plantslog/"+i+".tre"
-        infmonofn = "plantslog/"+i+".inf_mono"
+        generallogfileloc = "plantslog2/"+i+".log"
+        ttfntreefn = "plantslog2/"+i+".tre"
+        infmonofn = "plantslog2/"+i+".inf_mono"
         load_synth_extract.run_load_single_ttfn_inf_mono(dott,dload,studyloc,tstudy_list,javapre,
                                                 treemloc,generallogfileloc,dsynth,synthottolid,treefn,ttfntreefn,infmonofn)
 
