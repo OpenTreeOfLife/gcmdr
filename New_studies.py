2c05f6e48c99d5256bda35bcc27fd039faed8f6c
josephwb <phylo.jwb@gmail.com>
2014-04-17 15:58:36 -0400

    more from Bryan

--- New_studies.py
+++ New_studies.py
@@ -3,6 +3,15 @@ these are the studies that may go into the next round of synthesis
 """
 
 studytreelist=[
+
+## New from Bryan
+"2877_6671", # Campanulaceae; Lots of non-monopylly
+"1898_3874", # Delphinium; Lots of non-monopylly
+"244_3855",  # Gnetum, generic level
+"412_2166",  # Coniferophyta; 492 taxa; almost all genera monophyletic
+"2878_6673", # Lepechinia; generic level
+"2879_6674", # almost all Bryophyta genera
+
 ## For synthesis
 "251_134", # Formicidae. Moreau et al. 2006. Science
 "2881_6680", # Anolis. Mahler et al. 2013
