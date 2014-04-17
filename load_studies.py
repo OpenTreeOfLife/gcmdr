d6973b97cfc898806cf573bc871912f13afc1458
blackrim <blackrim@gmail.com>
2014-04-17 12:54:30 -0400

    plants added

--- load_studies.py
+++ load_studies.py
@@ -1,19 +1,19 @@
 import load_synth_extract
 
-from plants import studytreelist
+from microbes import studytreelist
 
 if __name__ == "__main__":
     from stephen_desktop_conf_TEMP2 import *
     
     synthottolid="10218"
-    studytreelist = ["2539_6294"]
+    #studytreelist = ["1296_2604"]
     print "loading synthottolid:",synthottolid
     print "loading studytreelist:",studytreelist
     
     for i in studytreelist:
         tstudy_list = [i]
-        generallogfileloc = "plantslog/"+i+".log"
-        ttfntreefn = "plantslog/"+i+".tre"
+        generallogfileloc = "microbeslog/"+i+".log"
+        ttfntreefn = "microbeslog/"+i+".tre"
         load_synth_extract.run_load_single_ttfn(dott,dload,studyloc,tstudy_list,javapre,
                                                 treemloc,generallogfileloc,dsynth,synthottolid,treefn,ttfntreefn)
 
--- load_studies_inf_mono.py
+++ load_studies_inf_mono.py
@@ -1,6 +1,6 @@
 import load_synth_extract
 
-from plants_all import studytreelist
+from microbes import studytreelist
 
 if __name__ == "__main__":
     from stephen_desktop_conf_TEMP import *
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     
     for i in studytreelist:
         tstudy_list = [i]
-        generallogfileloc = "plantslog_inf_mono/"+i+".log"
-        ttfntreefn = "plantslog_inf_mono/"+i+".tre"
-        infmonofn = "plantslog_inf_mono/"+i+".inf_mono"
+        generallogfileloc = "microbeslog_inf_mono/"+i+".log"
+        ttfntreefn = "microbeslog_inf_mono/"+i+".tre"
+        infmonofn = "microbeslog_inf_mono/"+i+".inf_mono"
         load_synth_extract.run_load_single_ttfn_inf_mono(dott,dload,studyloc,tstudy_list,javapre,
                                                 treemloc,generallogfileloc,dsynth,synthottolid,treefn,ttfntreefn,infmonofn)
 
