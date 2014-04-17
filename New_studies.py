2b05d9974890f714d3b8147f53660d24cbc5ef1e
josephwb <phylo.jwb@gmail.com>
2014-04-17 15:57:04 -0400

    minor

--- New_studies.py
+++ New_studies.py
@@ -3,8 +3,11 @@ these are the studies that may go into the next round of synthesis
 """
 
 studytreelist=[
-
+## For synthesis
 "251_134", # Formicidae. Moreau et al. 2006. Science
+"2881_6680", # Anolis. Mahler et al. 2013
 
+## Decent, but not necessarily for synthesis
+"2880_6676", # Stramenopiles. Brown and Sorhannus. 2010. PLoS ONE
 
 ]
