f5ca905ded898cc05ecf90cccba336565ba7be80
blackrim <blackrim@gmail.com>
2014-04-17 14:32:06 -0400

    updated

--- load_studies.py
+++ load_studies.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     from stephen_desktop_conf_TEMP2 import *
     
     synthottolid="10218"
-    #studytreelist = ["1296_2604"]
+    studytreelist = ["313_6019"]
     print "loading synthottolid:",synthottolid
     print "loading studytreelist:",studytreelist
     
     for i in studytreelist:
         tstudy_list = [i]
-        generallogfileloc = "microbeslog/"+i+".log"
-        ttfntreefn = "microbeslog/"+i+".tre"
+        generallogfileloc = "microbes_all_log/"+i+".log"
+        ttfntreefn = "microbes_all_log/"+i+".tre"
         load_synth_extract.run_load_single_ttfn(dott,dload,studyloc,tstudy_list,javapre,
                                                 treemloc,generallogfileloc,dsynth,synthottolid,treefn,ttfntreefn)
 
