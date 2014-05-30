156fe574c39c6b8aa581c5ef1922b1345fe9ba6f
Dail Laughinghouse <laughinghouseh@gmail.com>
2014-05-30 12:53:29 -0400

    Update New_studies-microbes.py

--- New_studies-microbes.py
+++ New_studies-microbes.py
@@ -5,6 +5,15 @@ these are the studies that may go into the next round of synthesis
 studytreelist=[
 
 # here is an example:
-"2862_6649", # ciliates. Feng et al. 2014. MPE
+"2862_6649", # ciliates. Gao et al. 2014. MPE
+"2863_6650", #ciliates. Gao et al. 2013. MPE
+"2739_6601", #Cyanobacteria. Schirrmeister et al. BMC Evol. Biol. 2011.
+"2892_6700", #cyanobacteria. Rigonato et al. Env. Microbio. 2013. 
+"2891_6699", #cyanobacteria. Rigonato et al. FEMS Microbiol Eco 2012.
+"2884_6688", #Mycobacterium. Sassi&Drancourt. BMC Genomics 2014.
+"2744_6343", #Dinoflagellata. Haywood et al. J Phycol. 2003.
+"2882_6682", #Mycobacterium. Mendum et al. BMC Genomics 2014
+"2770_6417", #Plasmodium. Ollomo et al. PLoS Pathogens. 2009
+"2765_6399", #Bosminidae. Taylor et al. Limnology & Oceano. 2002.
 
 ]
