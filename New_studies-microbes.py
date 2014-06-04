93b12dfa755e75d6dc268871fd662a487a6f1a6b
josephwb <phylo.jwb@gmail.com>
2014-06-04 15:44:00 -0400

    notes on new microbe studies

--- New_studies-microbes.py
+++ New_studies-microbes.py
@@ -4,16 +4,16 @@ these are the studies that may go into the next round of synthesis
 
 studytreelist=[
 
-# here is an example:
-"2862_6649", # Scuticociliatia (Ciliates). Gao et al. 2014. MPE
-"2863_6650", # Oligohymenophorea (Ciliates). Gao et al. 2013. MPE
+
+"2862_6649", # Scuticociliatia (Ciliates). Gao et al. 2014. MPE					## currently unrooted
+"2863_6650", # Oligohymenophorea (Ciliates). Gao et al. 2013. MPE				## currently unrooted
 "2739_6601", # blue-green algae (Cyanobacteria). Schirrmeister et al. BMC Evol. Biol. 2011.
-"2892_6700", # blue-green algae (Cyanobacteria). Rigonato et al. Env. Microbio. 2013. 
-"2891_6699", # blue-green algae (Cyanobacteria). Rigonato et al. FEMS Microbiol Eco 2012.
+"2892_6700", # blue-green algae (Cyanobacteria). Rigonato et al. Env. Microbio. 2013.		## currently unrooted
+"2891_6699", # blue-green algae (Cyanobacteria). Rigonato et al. FEMS Microbiol Eco 2012.	## currently unrooted
 "2884_6688", # Mycobacterium. Sassi and Drancourt. BMC Genomics 2014.
 "2744_6343", # Dinophyceae. Haywood et al. J Phycol. 2003.
 "2882_6682", # Mycobacterium. Mendum et al. BMC Genomics 2014
-"2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009
+"2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009					## currently unrooted
 "2765_6399", # Bosminidae. Taylor et al. Limnology and Oceano. 2002.
 
 ]
