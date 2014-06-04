ca0861e5cfdfa8219aedc94651a0390749844a01
josephwb <phylo.jwb@gmail.com>
2014-06-04 14:57:13 -0400

    minor cleanup of files

--- run_Mammals.py
+++ run_Mammals.py
@@ -4,30 +4,10 @@ this is the stuff specific to the studies that are loaded. It can include commen
 
 from wopr_conf import *
 
-## for testing synthesis:
-#from goo import studytreelist as goolist
 from mammals import studytreelist as mammalslist
-#from birds import studytreelist as birdslist
-#from tetrapoda import studytreelist as tetrapodalist
-#from decapods import studytreelist as decapodlist
-
-## for serious synthesis:
-#from plants import studytreelist as plantslist
-#from metazoa import studytreelist as metalist
-#from fungi import studytreelist as fungilist
-#from microbes import studytreelist as microbelist
 
 studytreelist = []
-#studytreelist.extend(goolist)
 studytreelist.extend(mammalslist)
-#studytreelist.extend(birdslist)
-#studytreelist.extend(tetrapodalist)
-#studytreelist.extend(decapodlist)
-
-#studytreelist.extend(plantslist)
-#studytreelist.extend(metalist)
-#studytreelist.extend(fungilist)
-#studytreelist.extend(microbelist)
 
 import load_synth_extract
 
@@ -35,7 +15,7 @@ import load_synth_extract
 # 229562 = Tetrapoda; Amniota = 229560; Archosauria = 335588; 304358 = Eukaryota; Decapoda = 169205;
 # 93302 = cellular organisms;
 
-synthottolid = "691846"
+synthottolid = "93302"
 #extractottolid = "691846"
 #synthottolid = "691846"
 
@@ -50,10 +30,7 @@ print "loading synthottolid:",synthottolid
 print "loading studytreelist:",studytreelist
 
 # analysis name
-#aname="Metazoa_test_2.5draft1_mapcompat"
-#aname="Life_taxonomy-only_2.5draft1"
-#aname="Decapoda_OTT2.5draft1_taxonomy-only"
-aname="Mammals_v2.5draft1_mapcompat"
+aname="Mammals_v2.6_mapcompat_18April"
 # location of synth database
 dsynth=bdir+aname+".db"
 # location of synth treefile
