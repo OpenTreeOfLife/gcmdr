f25ba29fdae3f71eb7ea24adcd081272550e0e8c
Dail Laughinghouse <laughinghouseh@gmail.com>
2014-06-04 23:30:14 -0400

    Update New_studies-microbes.py

--- New_studies-microbes.py
+++ New_studies-microbes.py
@@ -16,4 +16,39 @@ studytreelist=[
 "2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009					## currently unrooted
 "2765_6399", # Bosminidae. Taylor et al. Limnology and Oceano. 2002.
 "2749_6353", # Chlorophyta. Tartar et al. 2002
+"2886_6694", # Holozoa. 
+"2792_6478", # Diatom
+"2790_6476", # Moraxella
+"2888_6697", # Rhizobium
+"2885_6690", # Life
+"2885_6692", #	Life
+"2787_6704", #	Bacteria
+"2759_6374", #	Apicomplexa
+"2883_6686", #	Bacillus marcescens
+"2793_6705", #	Cercozoa
+"2748_6351", #	Sphacelariales
+"2889_6698", #	Bacteria
+"2745_6345", #	Dinophysis
+"2775_6432", #	Opisthokonta
+"2750_6355", #	Botryococcus
+"2895_6706", # 	Bacteria
+"2895_6707", #	Bacteria
+"2880_6676",	#Stramenopiles
+"2484_6607",	#Amoebozoa
+"2557_6631", #	Dinoflagellata
+"2557_6639", #	Dinoflagellata
+"2737_6322", #	Nostocales
+"2738_6323", #	Oscillatoriales
+"2738_6324", #	Oscillatoriales
+"310_259", #	Eukaryota
+"2760_6375",	#Dinoflagellates
+"2846_6608",	#Bacteriodetes
+"2847_6609",#	Eukaryota
+"2847_6611", #	Eukaryota
+"2925_2925", #	Eukaryota
+"2618_6130", #	Dinoflagellata
+"2927_6760", #	Pseudokeronopsidae
+"2772_6421", #	Thalassiosirales
+
+
 ]
