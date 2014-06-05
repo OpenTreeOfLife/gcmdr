9467ccfd2eb28277bc564dcd3539766cb2437d7b
josephwb <phylo.jwb@gmail.com>
2014-06-05 10:47:47 -0400

    edit to new microbes

--- New_studies-microbes.py
+++ New_studies-microbes.py
@@ -5,20 +5,21 @@ these are the studies that may go into the next round of synthesis
 studytreelist=[
 
 
-"2862_6649", # Scuticociliatia (Ciliates). Gao et al. 2014. MPE					## currently unrooted
-"2863_6650", # Oligohymenophorea (Ciliates). Gao et al. 2013. MPE				## currently unrooted
+"2862_6649", # Scuticociliatia (Ciliates). Gao et al. 2014. MPE					## unrooted
+"2863_6650", # Oligohymenophorea (Ciliates). Gao et al. 2013. MPE				## unrooted; no ingroup defined
 "2739_6601", # blue-green algae (Cyanobacteria). Schirrmeister et al. BMC Evol. Biol. 2011.
-"2892_6700", # blue-green algae (Cyanobacteria). Rigonato et al. Env. Microbio. 2013.		## currently unrooted
-"2891_6699", # blue-green algae (Cyanobacteria). Rigonato et al. FEMS Microbiol Eco 2012.	## currently unrooted
-"2884_6688", # Mycobacterium. Sassi and Drancourt. BMC Genomics 2014.
+"2892_6700", # blue-green algae (Cyanobacteria). Rigonato et al. Env. Microbio. 2013.		## unrooted; no ingroup defined
+"2891_6699", # blue-green algae (Cyanobacteria). Rigonato et al. FEMS Microbiol Eco 2012.	## unrooted; no ingroup defined
+"2884_6688", # Mycobacterium. Sassi and Drancourt. BMC Genomics 2014.				## no ingroup defined
 "2744_6343", # Dinophyceae. Haywood et al. J Phycol. 2003.
-"2882_6682", # Mycobacterium. Mendum et al. BMC Genomics 2014
-"2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009					## currently unrooted
-"2765_6399", # Bosminidae. Taylor et al. Limnology and Oceano. 2002.
+"2882_6682", # Mycobacterium. Mendum et al. BMC Genomics 2014					## no ingroup defined
+"2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009					## unrooted; no ingroup defined
+"2765_6399", # Bosminidae. Taylor et al. Limnology and Oceano. 2002.				## no ingroup defined
 "2749_6353", # Chlorophyta. Tartar et al. 2002
-"2886_6694", # Holozoa. 
-"2792_6478", # Diatom
-"2790_6476", # Moraxella
+
+"2886_6694", # Holozoa. Scheel and Hausdorf. 2014. MPE						## no ingroup defined
+"2792_6478", # Diatom. Ruck and Theriot. 2011. Protist						## no ingroup defined
+"2790_6476", # Moraxella. Angelos et al. 2007. Syst. Evol. Microbiol.				## no ingroup defined
 "2888_6697", # Rhizobium
 "2885_6690", # Life
 "2885_6692", #	Life
