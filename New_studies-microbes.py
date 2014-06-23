8b7dcf6acdcf73058437356cec91a6845062cc0a
Joseph W. Brown <phylo.jwb@gmail.com>
2014-06-23 10:37:40 -0400

    add pg_ prefix

--- New_studies-microbes.py
+++ New_studies-microbes.py
@@ -7,51 +7,51 @@ studytreelist=[
 
 # Hi Dail!
 
-"2862_6766", # Scuticociliatia (Ciliates). Gao et al. 2014. MPE					
-"2863_6767", # Oligohymenophorea (Ciliates). Gao et al. 2013. MPE			
-"2739_6601", # blue-green algae (Cyanobacteria). Schirrmeister et al. BMC Evol. Biol. 2011.
-"2892_6700", # blue-green algae (Cyanobacteria). Rigonato et al. Env. Microbio. 2013.		## unrooted; no ingroup defined
-"2891_6699", # blue-green algae (Cyanobacteria). Rigonato et al. FEMS Microbiol Eco 2012.	## unrooted; no ingroup defined
-"2884_6688", # Mycobacterium. Sassi and Drancourt. BMC Genomics 2014.				## no ingroup defined
-"2744_6343", # Dinophyceae. Haywood et al. J Phycol. 2003.
-"2882_6682", # Mycobacterium. Mendum et al. BMC Genomics 2014					
-"2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009					## unrooted; no ingroup defined
-"2765_6399", # Bosminidae. Taylor et al. Limnology and Oceano. 2002.				## no ingroup defined
-"2749_6353", # Chlorophyta. Tartar et al. 2002
+"pg_2862_6766", # Scuticociliatia (Ciliates). Gao et al. 2014. MPE					
+"pg_2863_6767", # Oligohymenophorea (Ciliates). Gao et al. 2013. MPE			
+"pg_2739_6601", # blue-green algae (Cyanobacteria). Schirrmeister et al. BMC Evol. Biol. 2011.
+"pg_2892_6700", # blue-green algae (Cyanobacteria). Rigonato et al. Env. Microbio. 2013.		## unrooted; no ingroup defined
+"pg_2891_6699", # blue-green algae (Cyanobacteria). Rigonato et al. FEMS Microbiol Eco 2012.	## unrooted; no ingroup defined
+"pg_2884_6688", # Mycobacterium. Sassi and Drancourt. BMC Genomics 2014.				## no ingroup defined
+"pg_2744_6343", # Dinophyceae. Haywood et al. J Phycol. 2003.
+"pg_2882_6682", # Mycobacterium. Mendum et al. BMC Genomics 2014					
+"pg_2770_6417", # Plasmodium. Ollomo et al. PLoS Pathogens. 2009					## unrooted; no ingroup defined
+"pg_2765_6399", # Bosminidae. Taylor et al. Limnology and Oceano. 2002.				## no ingroup defined
+"pg_2749_6353", # Chlorophyta. Tartar et al. 2002
 
-"2886_6694", # Holozoa. Scheel and Hausdorf. 2014. MPE						## no ingroup defined
-"2792_6478", # Diatom. Ruck and Theriot. 2011. Protist						## no ingroup defined
-"2790_6476", # Moraxella. Angelos et al. 2007. Syst. Evol. Microbiol.				## no ingroup defined
-"2888_6697", # Rhizobium. Althabegoiti et al. 2013. BMC Microbiol.				## no ingroup defined
-"2885_6690", # Life. Gu et al. 2014. BMC Evol. Biol.						## trees need informative names; no ingroup defined
-"2885_6692", # Life. Gu et al. 2014. BMC Evol. Biol.						## trees need informative names; no ingroup defined
-"2787_6704", # Bacteria. Fory et al. 2013. ???							## *** can't find publication ***
-"2759_6374", # Apicomplexa. Morrison. 2006. Adv. Parasit.					## unrooted
-"2883_6686", # Bacillus marcescens. Gillis et al. 2014. Eur. J. Plant Pathol.
-"2793_6705", # Cercozoa. Ota et al. 2012. Protist
-"2748_6351", # Sphacelariales. Heesch et al. 2008. Eur. J. Phycol.				## tree needs better name
-"2889_6698", # Bacteria. Lei and Olival. 2014. PLoS Negl. Trop. Dis.				## tree needs better name
-"2745_6345", # Dinophysis. Hackett et al. 2003. J. Phycol.
-"2775_6770", # Mesomycetozoa. Marshall et al. 2008. Protist					
-"2750_6355", # Botryococcus. Weiss et al. 2010. J. Phycol.
-"2895_6706", # Bacteria. Whitesonet al. 2014. BMC Genomics
-"2895_6707", # Bacteria. Whitesonet al. 2014. BMC Genomics
-"2880_6676", # Stramenopiles. Brown and Sorhannus. 2010. PLoS ONE				## perfection ;)
-"2484_6607", # Amoebozoa. Lahr et al. 2011. PLoS ONE
-"2557_6631", # Dinoflagellata. Orr et al. 2012. PLoS ONE
-"2557_6639", # Dinoflagellata. Orr et al. 2012. PLoS ONE
-"2737_6322", # Nostocales. Werner et al. 2012. Phycologia
-"2738_6323", # Oscillatoriales. Kling et al. 2012. Fottea, Olomouc
-"2738_6324", # Oscillatoriales. Kling et al. 2012. Fottea, Olomouc				## unrooted
-"310_259",   # Eukaryota. Katz et al. 2011. Protist						
-"2760_6375", # Dinoflagellates. Bachvaroff et al. 2013 MPE					## tree needs better name
-"2846_6608", # Bacteriodetes. Becker et al. 2014. BMC Microbiol.
-"2847_6609", # Eukaryota. He et al. 2014. Curr. Biol.						## tree needs better name
-"2847_6611", # Eukaryota. He et al. 2014. Curr. Biol.						## tree needs better name
-"2925_2925", # Eukaryota. Grant and Katz. 2014. PLoS Curr. ToL					## tree needs better name; no ingroup defined
-"2618_6130", # Dinoflagellata. Craveiro et al. 2011. Protist				
-"2927_6760", # Pseudokeronopsidae. da Silva Paiva et al. 2014. PLoS ONE
-"2772_6421", # Thalassiosirales. Alverson et al. 2011. BMC Evol. Biol.				## tree needs better name; no ingroup defined
-"2928_6761", # Cymbellales. Nakov et al. 2014. Phycologia					## unrooted; tree needs better name; can't find publication
+"pg_2886_6694", # Holozoa. Scheel and Hausdorf. 2014. MPE						## no ingroup defined
+"pg_2792_6478", # Diatom. Ruck and Theriot. 2011. Protist						## no ingroup defined
+"pg_2790_6476", # Moraxella. Angelos et al. 2007. Syst. Evol. Microbiol.				## no ingroup defined
+"pg_2888_6697", # Rhizobium. Althabegoiti et al. 2013. BMC Microbiol.				## no ingroup defined
+"pg_2885_6690", # Life. Gu et al. 2014. BMC Evol. Biol.						## trees need informative names; no ingroup defined
+"pg_2885_6692", # Life. Gu et al. 2014. BMC Evol. Biol.						## trees need informative names; no ingroup defined
+"pg_2787_6704", # Bacteria. Fory et al. 2013. ???							## *** can't find publication ***
+"pg_2759_6374", # Apicomplexa. Morrison. 2006. Adv. Parasit.					## unrooted
+"pg_2883_6686", # Bacillus marcescens. Gillis et al. 2014. Eur. J. Plant Pathol.
+"pg_2793_6705", # Cercozoa. Ota et al. 2012. Protist
+"pg_2748_6351", # Sphacelariales. Heesch et al. 2008. Eur. J. Phycol.				## tree needs better name
+"pg_2889_6698", # Bacteria. Lei and Olival. 2014. PLoS Negl. Trop. Dis.				## tree needs better name
+"pg_2745_6345", # Dinophysis. Hackett et al. 2003. J. Phycol.
+"pg_2775_6770", # Mesomycetozoa. Marshall et al. 2008. Protist					
+"pg_2750_6355", # Botryococcus. Weiss et al. 2010. J. Phycol.
+"pg_2895_6706", # Bacteria. Whitesonet al. 2014. BMC Genomics
+"pg_2895_6707", # Bacteria. Whitesonet al. 2014. BMC Genomics
+"pg_2880_6676", # Stramenopiles. Brown and Sorhannus. 2010. PLoS ONE				## perfection ;)
+"pg_2484_6607", # Amoebozoa. Lahr et al. 2011. PLoS ONE
+"pg_2557_6631", # Dinoflagellata. Orr et al. 2012. PLoS ONE
+"pg_2557_6639", # Dinoflagellata. Orr et al. 2012. PLoS ONE
+"pg_2737_6322", # Nostocales. Werner et al. 2012. Phycologia
+"pg_2738_6323", # Oscillatoriales. Kling et al. 2012. Fottea, Olomouc
+"pg_2738_6324", # Oscillatoriales. Kling et al. 2012. Fottea, Olomouc				## unrooted
+"pg_310_259",   # Eukaryota. Katz et al. 2011. Protist						
+"pg_2760_6375", # Dinoflagellates. Bachvaroff et al. 2013 MPE					## tree needs better name
+"pg_2846_6608", # Bacteriodetes. Becker et al. 2014. BMC Microbiol.
+"pg_2847_6609", # Eukaryota. He et al. 2014. Curr. Biol.						## tree needs better name
+"pg_2847_6611", # Eukaryota. He et al. 2014. Curr. Biol.						## tree needs better name
+"pg_2925_2925", # Eukaryota. Grant and Katz. 2014. PLoS Curr. ToL					## tree needs better name; no ingroup defined
+"pg_2618_6130", # Dinoflagellata. Craveiro et al. 2011. Protist				
+"pg_2927_6760", # Pseudokeronopsidae. da Silva Paiva et al. 2014. PLoS ONE
+"pg_2772_6421", # Thalassiosirales. Alverson et al. 2011. BMC Evol. Biol.				## tree needs better name; no ingroup defined
+"pg_2928_6761", # Cymbellales. Nakov et al. 2014. Phycologia					## unrooted; tree needs better name; can't find publication
 
 ]
