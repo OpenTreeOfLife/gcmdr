dc5fae2542ac36eea44064202a775ab7c3754600
josephwb <phylo.jwb@gmail.com>
2014-06-25 11:12:41 -0400

    more JWB stuff

--- load_mls_JWB.py
+++ load_mls_JWB.py
@@ -11,20 +11,28 @@ java -jar /home/josephwb/Work/OToL/treemachine/target/treemachine-0.0.1-SNAPSHOT
 
 #from goo import studytreelist as goolist
 #from mammals import studytreelist as mammalslist
-from birds import studytreelist as birdslist
+#from birds import studytreelist as birdslist
 #from tetrapoda import studytreelist as tetrapodalist
 #from decapods import studytreelist as decapodlist
 #from plants import studytreelist as plantslist
-#from metazoa import studytreelist as metalist
+from metazoa import studytreelist as metalist
 #from fungi import studytreelist as fungilist
 #from microbes import studytreelist as microbelist
 
 studytreelist = []
 
-studytreelist.extend(birdslist)
+#studytreelist.extend(goolist)
+#studytreelist.extend(mammalslist)
+#studytreelist.extend(birdslist)
+#studytreelist.extend(tetrapodalist)
+#studytreelist.extend(decapodlist)
+#studytreelist.extend(plantslist)
+studytreelist.extend(metalist)
+#studytreelist.extend(fungilist)
+#studytreelist.extend(microbelist)
 
 taxtreefile = "tax.tree"
-aname = "Aves"
+aname = "Metazoa"
 mlsout = aname + "_mls.NEX"
 tgenerallogfileloc = "/home/josephwb/TEMP/"
 
