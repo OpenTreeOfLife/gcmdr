0fa3fa3c200b7385ed25001725e045ce4507c2a6
blackrim <blackrim@gmail.com>
2014-06-25 11:43:51 -0400

    mls

---
+++ files_for_submission_v2.0/MLS_MPR_files/asterids.py
@@ -0,0 +1,95 @@
+studytreelist=[
+               "pg_2624_6139", #Veronica
+               "pg_2128_4437",#Plantago
+               "pg_1102_2177",#Collinsia
+               "pg_1129_2251", #  Solanum
+               "pg_1109_2201", #  Castilleja
+               "pg_2004_4118",#cyrtandra
+               "pg_53_1280", #  Euryops
+               "pg_1118_2226", #Mentheae,lamiaceae
+               "pg_2669_6213", #Lamiaceae
+               "pg_2032_5922",#Ruella
+               "pg_1901_3877",#Lentibulariaceae
+               "pg_713_1287", #Lamiales
+               "pg_2539_6294",#Soltis et al. 2011 ML tree
+ 	       "pg_2820_6566",#Streptophyta
+               "pg_259_142", #Cercis FABALES!
+               "pg_264_150", #Coursetia FABALES!
+               "pg_267_161", #Ateleia (Swartzieae-Leguminosae) FABALES!
+               "pg_2077_4291", #Podalyria (Fabaceae, Podalyrieae) FABALES!
+               "pg_293_201", #Mimosa FABALES!
+               "pg_197_784", #Phaseolus FABALES!
+               "pg_595_896", #Senna FABALES!
+               "pg_131_6236", #Trifolium FABALES!
+               "pg_2689_6241", #Lupinus FABALES!
+               "pg_597_906", #Machaerium (Leguminosae) FABALES!
+               "pg_2001_4100", #Astragalus FABALES!
+               "pg_606_5290", #Trifolieae and Vicieae FABALES!
+               "pg_270_159", #Indigofereae FABALES!
+               "pg_596_901", #Genisteae (Leguminosae) FABALES!
+               "pg_294_202", #Detarieae (Caesalpinioideae) FABALES!
+               "pg_292_199", #(Diocleinae: Papilionoideae) FABALES!
+	       "pg_58_775", #Crotalarieae (Fabaceae) FABALES!
+               "pg_548_798", #Vigna FABALES!
+               "pg_2055_4234",  #Genistoid legumes FABALES!
+               "pg_2057_4240", #papilionoid FABALES!
+               "pg_2127_4426", #Papilionoideae; Vataireoid Clade FABALES!
+               "pg_594_890", #robinioid legumes FABALES!
+               "pg_261_145", #Caesalpinieae FABALES!
+               "pg_57_777", #Podalyrieae (Fabaceae) FABALES!
+               "pg_1087_2114", #phaseoloid FABALES!
+               "pg_1087_2115", #phaseoloid FABALES!
+               "pg_2690_6243", #Fabaceae FABALES!
+               "pg_2045_4213", #Acacia FABALES!
+               "pg_605_947", #Strophostyles (Fabaceae) FABALES!
+               "pg_271_5017", #Polygalaceae FABALES!
+               "pg_2661_6198", #Ericales
+               "pg_2052_4228",#Lundia
+               "pg_1103_2178",#Bignonieae
+               "pg_14_12", #Bignoniaceae
+               "pg_2598_6020",#Boraginaceae
+               "pg_2000_4098",#Coffea
+               "pg_20_2162",#Gallium
+               "pg_1101_2172",#Rubieae
+               "pg_2565_5708",#Ericoideae
+               "pg_1094_2138",#Apocynaceae
+               "pg_2641_6160",#Rubiaceae
+               "pg_99_5885",#Barnadesioideae
+               "pg_93_1411",#Symplocos
+               "pg_898_1732",#Schefflera
+               "pg_216_5865",#Hedera
+               "pg_901_1740",#Meryta
+               "pg_2830_6583",#brassaiopsis
+               "pg_2831_6584",#Escallonia
+               "pg_719_1296",#Nymphoides
+               "pg_1975_4041",#Tragopogon
+               "pg_1821_3678",#Helichrysum
+               "pg_1583_3194",#Gaillardia
+               "pg_1581_3188",#Dubautia
+               "pg_1575_3164",#Tolpis
+               "pg_1573_3144",#Onoseris
+               "pg_934_1832",#Echinops
+               "pg_200_6585",#Encelia
+               "pg_152_5743",#Coreopsis
+               "pg_53_1281",#Euryops
+               "pg_2076_4282",#Garrya 
+               "pg_50_1397",#Anagallis 
+               "pg_80_5881",#Rhododendron
+               "pg_82_5792",#Campanula
+               "pg_1962_6580",#Viburnum Clement and Donoghue 2011
+               "pg_915_1802",#Viburnum
+               "pg_915_1803",#Valerianaceae
+               "pg_2625_6140",#Utricularia
+               "pg_1130_2258",#Nicotiana
+               "pg_2047_4217",#Cuscuta
+               "pg_386_459",#Brunsfelisia
+               "pg_139_5860",#Nierembergia
+               "pg_126_2233",#Solanum
+               "pg_136_5857",#cestrum               
+               "pg_09_1",#Campanulidae
+               "pg_2828_6578",#caprifolieae Smith 2009 NEW
+               "pg_142_38",#Asclepias
+               "pg_21_37",#Solanaceae
+               "pg_75_1743",#Apioideae
+               "pg_41_1396"#Feddea
+               ]
---
+++ files_for_submission_v2.0/MLS_MPR_files/core_eudicots.py
@@ -0,0 +1,25 @@
+studytreelist=[
+               "pg_2827_6577",#Ilex NEW
+               "pg_761_1415", #   Drosera
+               "pg_754_1392", #   Ribes
+               "pg_330_325", #    Santalum
+               "pg_77_5878", #  Anaxagorea,MAGNOLIIDS
+               "pg_1131_2265", #Saxifragaceae
+               "pg_2608_6288", #saxifrigales
+               "pg_2539_6294",#Soltis et al. 2011 ML tree
+ 	       "pg_2820_6566",#Streptophyta
+               "pg_2645_6165",#Menispermaceae
+               "pg_2644_6164",#Ranunculales
+               "pg_2642_6161",#Cayophyllales; not sure if you have a better study here)
+               "pg_2140_4483",#Annonaceae,MAGNOLIIDS
+               "pg_2626_6142",#Amaranthaceae
+               "pg_30_2281",#Illicium, EARLY ANGIO
+               "pg_332_333", #    Polygonaceae
+               "pg_2832_6586",#Sedum    
+               "pg_1866_3765",#Thesium (Santalaceae)
+               "pg_59_5731",#Aristolochiaceae,MAGNOLIIDS
+               "pg_704_1266",#Molluginaceae
+               "pg_2638_6157",#Santalales
+               "pg_1974_4038",#PolygonaceaeS
+               "pg_1974_4039"#Rheum
+               ]
