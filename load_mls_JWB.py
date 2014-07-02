eb7f0da2c94e4d8460c4faf6db2929bf5f18e9fc
josephwb <phylo.jwb@gmail.com>
2014-07-02 14:22:42 -0400

    JWB files only

--- load_mls_JWB.py
+++ load_mls_JWB.py
@@ -6,15 +6,15 @@ import sys
 sequence of commands:
 1) run this file: python load_mls_JWB.py
 2) perform MLS ANALYSIS: run.sh <aname>_mls.NEX
-3) run this file: python mls_posttree_fix.py <in treename> <out treename>
-4) convert OTTIDs back to names: java -jar /home/josephwb/Work/OToL/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar labeltipsottol TREEFILE /home/josephwb/Work/OToL/treemachine/Synthesis/Life.2.8draft5.db > TREEFILEOUT
+3) run this file: python mls_posttree_fix_JWB.py <in treename> <out treename>
+4) convert OTTIDs back to names: java -jar /home/josephwb/Work/OToL/treemachine/target/treemachine-0.0.1-SNAPSHOT-jar-with-dependencies.jar labeltipsottol TREEFILE /home/josephwb/Work/OToL/treemachine/Synthesis/Life.2.8draft5.db  | tail -n +2 > TREEFILEOUT
 """
 
 sys.path.insert(0, 'files_for_submission_v2.0')
 
 #from goo import studytreelist as goolist
 #from mammals import studytreelist as mammalslist
-from birds import studytreelist as birdslist
+#from birds import studytreelist as birdslist
 #from tetrapoda import studytreelist as tetrapodalist
 #from decapods import studytreelist as decapodlist
 #from plants import studytreelist as plantslist
@@ -22,11 +22,11 @@ from birds import studytreelist as birdslist
 #from fungi import studytreelist as fungilist
 #from microbes import studytreelist as microbelist
 
-studytreelist = []
+studytreelist = ["pg_420_522", "pg_2015_4152"]
 
 #studytreelist.extend(goolist)
 #studytreelist.extend(mammalslist)
-studytreelist.extend(birdslist)
+#studytreelist.extend(birdslist)
 #studytreelist.extend(tetrapodalist)
 #studytreelist.extend(decapodlist)
 #studytreelist.extend(plantslist)
@@ -35,7 +35,7 @@ studytreelist.extend(birdslist)
 #studytreelist.extend(microbelist)
 
 taxtreefile = "tax.tree"
-aname = "Aves"
+aname = "Aves_contrived"
 mlsout = aname + "_mls.NEX"
 tgenerallogfileloc = "/home/josephwb/TEMP/"
 
