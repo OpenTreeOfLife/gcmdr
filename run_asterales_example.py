a044a03fa5ed8630601fbc6f81e9d07aa342573c
josephwb <phylo.jwb@gmail.com>
2014-07-17 11:45:12 -0400

    fix study 9

--- run_asterales_example.py
+++ run_asterales_example.py
@@ -8,7 +8,7 @@ from general_utils import *
 studytreelist=["pg_2539_6294", #Soltis et al. 2011
                "pg_715_1289",  #Barnadesioideae
                "pg_329_324",   #Hieracium
-        #       "pg_9_1",       #Campanulidae
+               "9_1",       #Campanulidae
                "pg_709_1276",  #Lobelioideae, very non monophyletic
                "pg_41_1396",   #Feddea
                "pg_82_5792",   #Campanula, very non monophyletic campanula
